762e3ae90f4a0400807c57f70300e736d727a2eb
M9M9Ra <vasa4g@gmail.com>
2024-06-12 10:56:54 +0300

    #vue init $2

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,33 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  // pluginOptions: {
+  //   vuetify: {
+  //     // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+  //   }
+  // },
+
+  filenameHashing: false,
+  outputDir: 'dist',
+  publicPath: '/spa/dist/',
+  // publicPath: 'http://localhost:5000/',
+  configureWebpack: {
+    optimization: {
+      splitChunks: false,
+    },
+    output: {
+      filename: 'js/main.js',
+    },
+    devServer: {
+      port: 5000,
+      hot: true,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+      historyApiFallback: true,
+    },
+  }
+
 })
+
